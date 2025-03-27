5daae1bbae545d38321c764dbcefa34fcb2c6e61
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-03-27 15:19:37 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+print('welcome to python')
+x=3
+print("the value of x is",x)
+y="swastika"
+print("my name is",y)
+print("welcome to python",end="*") 
+import keyword 
+print("python keywords are \n")
+print(keyword.kwlist)
+x="swastika"
+y="bhargava"
+print("my name is",x+" "+y)
