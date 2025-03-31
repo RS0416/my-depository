d066c2467295883d8ddafef7feb33179504f0213
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-03-31 15:19:35 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,21 @@
+a=5
+b=2.2
+c="swastika"
+d=True
+print(d)
+print("data type of d is",type(d))
+print("the data type of a is",type(a))
+print("the data type of b is",type(b))
+print("the data type of c is",type(c))
+h=5.3
+w=35
+a=11
+print("the data type of height is",type(h))
+h=int(h)
+print("the data type of height is",type(h))
+print(h)
+x=2
+y=3
+print(str(x)+str(y))
+revtext=c[::-1]
+print(revtext)
