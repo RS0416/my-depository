b4e25a8f92ffdf02cbc1c97e9ba398c5dca105e2
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-04-25 15:40:00 +0530

    Add files via upload

---
+++ age check hw.py
@@ -0,0 +1,15 @@
+me=12
+bro=3
+mom=41
+if me>=20:
+    print("I am above 20.")
+else:
+    print("Iam below 20")
+if bro<10:
+    print("brother is below 10")
+else:
+    print("brother is above 10")
+if mom>=20:
+    print("mom is greater than 20")
+else:
+    print("mom is below 20")
