24c5050fcbf825ff3df78becd5aed1a4afcc6918
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-04-29 13:18:31 +0530

    Add files via upload

---
+++ no.swap2.py
@@ -0,0 +1,7 @@
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+c=int(input("enter the value:"))
+if a and b and c:
+    print("the no. are in order.")
+else:
+    print("the no. are not in order.")
