6fb1f2d4af434472ba6a0d716a5d3d7f80e4b6c4
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-05-02 15:51:44 +0530

    Add files via upload

--- no.swap2.py
+++ no.swap2.py
@@ -1,7 +1,8 @@
 a=int(input("enter the value:"))
 b=int(input("enter the value:"))
 c=int(input("enter the value:"))
-if a and b and c:
-    print("the no. are in order.")
-else:
-    print("the no. are not in order.")
+nv=a
+a=b
+b=c
+c=nv
+print("the swaping of three no. is",a,b,c)
