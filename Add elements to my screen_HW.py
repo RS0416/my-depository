077eaa863d3c97ced3bb919892e46d6964b2120e
RS0416 <149681600+RS0416@users.noreply.github.com>
2025-11-16 19:58:25 +0530

    Add files via upload

---
+++ Add elements to my screen_HW.py
@@ -0,0 +1,14 @@
+import pygame
+pygame.init()
+screen= pygame.display.set_mode((500,500))
+done= False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done= True
+    pygame.draw.rect(screen, (0, 125, 255), pygame.Rect(30,30,60,60))
+    pygame.draw.circle(screen, (0,255,0), (200,200),50)
+    pygame.draw.rect(screen,(255,255,0), (40,40,100,100))
+    pygame.draw.circle(screen, (255,0,255), (200,300),60,3)
+    pygame.display.flip()
