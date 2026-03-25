57ffe1245d93bbdf13d1c0e41698aef67d1d726e
RS0416 <149681600+RS0416@users.noreply.github.com>
2026-03-25 16:51:03 +0530

    Add files via upload

---
+++ denominations_tkinter.py
@@ -0,0 +1,97 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+root= Tk()
+root.title("denomination counter")
+root.configure(bg='light blue')
+root.geometry("650x400")
+upload= Image.open("app_img.jpg")
+upload= upload.resize((300,300))
+image= ImageTk.PhotoImage(upload)
+label= Label(root, image= image, bg="light blue")
+label.place(x=180, y=20)
+label1= Label(root,text= "hey user!, welcome to denomination applicaton counter!",
+    bg= 'light blue'
+)
+label1.place(relx=0.5, y=340, anchor= CENTER)
+def msg():
+    msgbox= messagebox.showinfo(
+        'alert'
+        "do you want to calculate the denomination count?"
+    )
+    if msgbox=="ok":
+        topwin()
+
+
+button1= Button(
+    root, 
+    text="lets get started",
+    command=msg,
+    bg="brown",
+    fg= "white"
+)
+button1.place(x=250, y=360)
+def topwin():
+    top= Toplevel()
+    top.titlte('denominations calculator')
+    top.configure(bg="light grey")
+    top.geometry('600x350+50+50')
+    label= Label(top, text="enter total amt", bg="light grey")
+    entry= Entry(top)
+
+    lbl= Label(
+        top,
+        text="here are no. of notesfor each denomination ..",
+        bg= "Light grey"
+    )
+
+    l1= Label(top, text= "2000", bg= "light grey")
+    l2= Label(top, text= "500", bg= "light grey")
+    l3= Label(top, text= "100", bg= "light grey")
+
+    t1=Entry(top)
+    t2=Entry(top)
+    t3=Entry(top)
+
+    def calculator():
+        try:
+            amt= int(entry.get())
+            note2000= amt // 2000
+            amt %= 2000
+            note500= amt // 500
+            amt %= 500
+            note100= amt // 100
+
+            t1.delete(0, END)
+            t2.delete(0, END)
+            t3.delete(0, END)
+
+            t1.insert(END, str(note2000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
+        except ValueError:
+            messagebox.showerror("error"," pls enter a valid no.")
+            
+    btn= Button(
+        top, 
+        text= "calculate",
+        command=calculator,
+        bg= 'brown',
+        fg= 'white'
+    )
+    
+    label.place(x=230, y=50)
+    entry.place(x=200, y=80)
+    btn.place(x=240, y=120)
+    lbl.place(x=140, y=170)
+    l1.place(x=180, y=200)
+    l2.place(x=180, y=230)
+    l3.place(x=180, y=260)
+    t1.place(x=270, y=200)
+    t2.place(x=270, y=230)
+    t3.place(x=270, y=260)
+
+    top.mainloop()
+
+
+root.mainloop()
