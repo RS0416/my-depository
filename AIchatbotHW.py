69e484428c0f0926617d0e0c2a7c2eb64b13cb70
RS0416 <149681600+RS0416@users.noreply.github.com>
2026-04-26 21:27:42 +0530

    Add files via upload

---
+++ AIchatbotHW.py
@@ -0,0 +1,17 @@
+print("what is your name?")
+name=input(":")
+print("wonderful!")
+print("what is your age?")
+age=int(input(":"))
+print("ooh")
+print("what is you favourite color?")
+fav_color= input(":")
+print("Nice choice!")
+print("How did school go today?(good/bad)")
+school_status= input("good/bad:").lower
+if school_status=="good":
+    print("Nice to know that about your school.")
+elif(school_status=="bad"):
+    print("Oh, I hope you will feeel better soon.")
+else:
+    print("I know it is hard to express your feelings sometimes.")
